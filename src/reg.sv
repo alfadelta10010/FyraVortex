0aa0aeabf2a000706c3d2f23c256e4c6cbbcc215
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-02 18:31:51 +0530

    added enable signal

--- src/reg.sv
+++ src/reg.sv
@@ -1,13 +1,16 @@
-module reg #(parameter N = 32) (clk, reset, in, out);
+module reg #(parameter N = 32) (clk, rst, in, out, en);
   input bit clk;
-  input bit reset;
+  input bit rst;
+  input bit en;
   input logic [N-1:0] in;
   output logic [N-1:0] out;
-  always_ff @(posedge clk, posedge reset) 
+  always_ff @(posedge clk or posedge rst) 
     begin
-      if (reset) 
-        out <= 32'b0;
-      else 
+      if (rst)
+        out <= 0;
+      else if (en) 
         out <= in;
-  end
+      else
+        out <= out;
+    end
 endmodule
