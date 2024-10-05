30b9ce8295efad89e39623ba3fe183367ce2b890
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-05 22:50:18 +0530

    var name refactoring

--- src/regFile.sv
+++ src/regFile.sv
@@ -1,27 +1,26 @@
-module RegFile #(parameter DATA_WIDTH = 32) (
-    input       clk,
-    input       wr_en,
-    input       [4:0] rd_addr1, rd_addr2, wr_addr,
-    input       [DATA_WIDTH-1:0] wr_data,
-    output      [DATA_WIDTH-1:0] rd_data1, rd_data2
-);
-
-logic [DATA_WIDTH-1:0] reg_file_arr [0:31];
-
-integer i;
-initial begin
-    for (i = 0; i < 32; i = i + 1) begin
-        reg_file_arr[i] = 0;
+module regFile #(parameter DATA_WIDTH = 32) (clk, wr_en, rs1, rs2, rd, r1, r2, dIn);
+  input logic clk, wr_en;
+  input logic [4:0] rs1, rs2, rd;
+  input logic [DATA_WIDTH-1:0] dIn;
+  output logic [DATA_WIDTH-1:0] r1, r2;
+  logic [DATA_WIDTH-1:0] reg_file [0:31];
+  integer i;
+  initial begin
+    for (i = 0; i < 32; i = i + 1) 
+      begin
+        reg_file[i] = 0;
+      end
+  end
+  
+  // register file write logic (synchronous)
+  always @(posedge clk) 
+    begin
+      if (wr_en) 
+        reg_file[rd] <= dIn;
     end
-end
-
-// register file write logic (synchronous)
-always @(posedge clk) begin
-    if (wr_en) reg_file_arr[wr_addr] <= wr_data;
-end
-
-// register file read logic (combinational)
-assign rd_data1 = ( rd_addr1 != 0 ) ? reg_file_arr[rd_addr1] : 0;
-assign rd_data2 = ( rd_addr2 != 0 ) ? reg_file_arr[rd_addr2] : 0;
-
+  // MAJOR ISSUE, r0 GETS OVERWRITTEN
+  
+  // register file read logic (combinational)
+  assign r1 = (rs1 != 0 ) ? reg_file[rs1] : 0;
+  assign r2 = (rs2 != 0 ) ? reg_file[rs2] : 0;
 endmodule
