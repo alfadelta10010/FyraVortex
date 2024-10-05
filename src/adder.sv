3881094fdd66739e5ee73d8c0fa932b9528af583
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-05 23:09:30 +0530

    added missing carry signals to adder

--- src/adder.sv
+++ src/adder.sv
@@ -1,7 +1,9 @@
 `timescale 1ns / 1ps
-module adder #(parameter N = 32)(a, b, s);
+module adder #(parameter N = 32)(a, b, s, cin, cout);
   input logic [N-1:0] a;
   input logic [N-1:0] b;
   output logic [N-1:0] s;
-  assign s = a + b;
+  input logic cin;
+  output logic cout;
+  assign {cout, s} = a + b + cin;
 endmodule
