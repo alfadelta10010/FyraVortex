baf1c1565148c96d13f261720e6d4c1983fa9a55
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-05 22:48:57 +0530

    fixed naming scheme

--- src/MEM_WB.sv
+++ src/MEM_WB.sv
@@ -20,7 +20,7 @@ module MEM_WB(clk, wb_ctrl, mem_rd, mem_wr, mem_ctrl, reg_wr, pc_out, alu_out, m
 	assign reg_wr_out = reg_wr;
 	assign rd_out = rd;
 	memCtrl #(12) Memory_Controller(.dataRI(mem_out), .dataRO(out), .addrIn(alu_out), .addrOut(addr_out), .dataWI(mem_data), .dataWO(mem_data_in), .mem_ctrl(mem_ctrl));
-	DataMem #(12) Data_Memory(.addr(addr_out), .dataW(mem_data_in), .dataR(mem_out), .memR(mem_rd), .memW(mem_wr), .clk(clk));
+	dataMem #(12) Data_Memory(.addr(addr_out), .dataW(mem_data_in), .dataR(mem_out), .memR(mem_rd), .memW(mem_wr), .clk(clk));
 	mux31   #(32) Write_Back_MUX(.a(pc_out), .b(alu_out), .c(out), .s(wb_ctrl), .y(wb_out));
 	
 endmodule
