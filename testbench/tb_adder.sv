8710fb26e82976029649e0655e29e6a9d3d39ab8
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 11:33:41 +0530

    modified for new adder

--- testbench/tb_adder.sv
+++ testbench/tb_adder.sv
@@ -2,25 +2,19 @@ module tb_adder();
   logic [31:0] a_tb;
   logic [31:0] b_tb;
   logic [31:0] s_tb;
-  logic cin_tb;
-  logic cout_tb;
   
-  adder #(32) dut (.a(a_tb), .b(b_tb), .cin(cin_tb), .s(s_tb), .cout(cout_tb));
+  adder #(32) dut (.a(a_tb), .b(b_tb), .s(s_tb));
   
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, tb_adder);
     a_tb = 32'h00000000;
     b_tb = 32'h00000004;
-    cin_tb = 0;
     #1 a_tb = 32'h00000004;
     b_tb = 32'h00000004;
-    cin_tb = 0;
     #1 a_tb = 32'h00000004;
     b_tb = 32'h0000F004;
-    cin_tb = 1;
     #1 a_tb = 32'hFFFFFFFE;
     b_tb = 32'h00000001;
-    cin_tb = 1;
   end
 endmodule
