9bee949553fca4a86cb6c5cbf88e2b7776cd1806
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 23:12:24 +0530

    refactored var names

--- src/branchCtrl.sv
+++ src/branchCtrl.sv
@@ -1,50 +1,50 @@
-module branchCtrl(b_control, r1, r2, branch_sel);
-  input logic [2:0] b_control;
+module branchCtrl(bCtrl, r1, r2, bSel);
+  input logic [2:0] bCtrl;
   input logic [31:0] r1;
   input logic [31:0] r2;
-  output logic [0:0] branch_sel;
+  output logic [0:0] bSel;
   always_comb
-    branch_sel = 1'b0;
+    bSel = 1'b0;
     begin
-      case(b_control)
+      case(bCtrl)
         3'b011: begin // BEQ
           if (r1 == r2)
-            branch_sel = 1'b1;
+            bSel = 1'b1;
           else
-            branch_sel = 1'b0;
+            bSel = 1'b0;
         end
         3'b001: begin // BNE
           if (r1 != r2)
-            branch_sel = 1'b1;
+            bSel = 1'b1;
           else
-            branch_sel = 1'b0;
+            bSel = 1'b0;
         end
         3'b100: begin // BLT
           if ($signed(r1) < $signed(r2))
-            branch_sel = 1'b1;
+            bSel = 1'b1;
           else
-            branch_sel = 1'b0;
+            bSel = 1'b0;
         end
         3'b110: begin // BLTU
           if (r1 < r2)
-            branch_sel = 1'b1;
+            bSel = 1'b1;
           else
-            branch_sel = 1'b0;
+            bSel = 1'b0;
         end
         3'b101: begin // BGE
           if ($signed(r1) >= $signed(r2))
-            branch_sel = 1'b1;
+            bSel = 1'b1;
           else
-            branch_sel = 1'b0;
+            bSel = 1'b0;
         end
         3'b111: begin // BGEU
           if (r1 >= r2)
-            branch_sel = 1'b1;
+            bSel = 1'b1;
           else
-            branch_sel = 1'b0;
+            bSel = 1'b0;
         end
         default: begin 
-          branch_sel = 1'b0;
+          bSel = 1'b0;
         end
       endcase
     end
