f01e5089b935ba9c38830dfee1c37ef9e22f916c
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 17:14:02 +0530

    updated signal names and avoided x0 writing

--- src/regFile.sv
+++ src/regFile.sv
@@ -1,11 +1,11 @@
-module regFile #(parameter DATA_WIDTH = 32) (clk, wr_en, rs1, rs2, rd, r1, r2, dIn);
+module regFile #(parameter DATA_WIDTH = 32) (clk, wrEn, rs1, rs2, rd, r1, r2, dIn);
   input logic clk, wr_en;
   input logic [4:0] rs1, rs2, rd;
   input logic [DATA_WIDTH-1:0] dIn;
   output logic [DATA_WIDTH-1:0] r1, r2;
-  logic [DATA_WIDTH-1:0] reg_file [0:31];
-  integer i;
+  reg [DATA_WIDTH-1:0] reg_file [0:31];
   initial begin
+    int i;
     for (i = 0; i < 32; i = i + 1) 
       begin
         reg_file[i] = 0;
@@ -14,11 +14,7 @@ module regFile #(parameter DATA_WIDTH = 32) (clk, wr_en, rs1, rs2, rd, r1, r2, d
   
   // register file write logic (synchronous)
   always @(posedge clk) 
-    begin
-      if (wr_en) 
-        reg_file[rd] <= dIn;
-    end
-  // MAJOR ISSUE, r0 GETS OVERWRITTEN
+    reg_file[rd] <= (wrEn && rd != 0) ? dIn : reg_file[rd];
   
   // register file read logic (combinational)
   assign r1 = (rs1 != 0 ) ? reg_file[rs1] : 0;
