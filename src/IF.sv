fded57051967e6714c0576421e20b316b939abbc
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 17:26:16 +0530

    changed signal names

--- src/IF.sv
+++ src/IF.sv
@@ -1,10 +1,11 @@
-module IF(pc_rst, clk, branch_sel, branch_val, pc_p, pc_n, inst_out, pc_en);
-  input logic pc_rst, clk, branch_sel, pc_en;
-  input logic [31:0] branch_val;
-  output logic [31:0] pc_p, pc_n, inst_out;
-  logic [31:0] pc_in;
-  adder #(32) pc_adder (.a(32'd4), .b(pc_p), .s(pc_n)); 
-  instMem #(32, 32, 512) inst_mem (.inst_addr(pc_p), .inst(inst_out));
-  mux21 #(32) branch_mux(.a(pc_n), .b(branch_val), .s(branch_sel), .y(pc_in));
-  regs #(32) pc(.clk(clk), .rst(pc_rst), .in(pc_in), .out(pc_p), .en(pc_en));
+module IF(pcRst, clk, branchSel, branchVal, pcP, pcN, instOut, pcEn);
+  input logic pc_rst, clk, branch_sel, pcEn;
+  input logic [31:0] branchVal;
+  output logic [31:0] pcP, pcN, instOut;
+  logic [31:0] pcIn;
+  
+  adder #(32) pcAdder (.a(32'd4), .b(pcP), .s(pcN)); 
+  instMem #(32, 32, 512) instructionMemory (.inst_addr(pcP), .inst(instOut));
+  mux21 #(32) branchMux (.a(pcN), .b(branchVal), .s(branchSel), .y(pcIn));
+  regs #(32) programCounter (.clk(clk), .rst(pcRst), .in(pcIn), .out(pcP), .en(pcEn));
 endmodule
