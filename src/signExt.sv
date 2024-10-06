bb34125941aed6568fa32e72a13c5b991be7f003
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 13:42:16 +0530

    formatting fix and default case update

--- src/signExt.sv
+++ src/signExt.sv
@@ -22,11 +22,11 @@ module signExt(opcode, instIn, immOut);
         immOut[11:0] = 12'b0;
       end
       7'b1101111: begin // JAL
-              immOut = {{20{instIn[31]}}, instIn[19:12], instIn[13], instIn[30:21], 1'b0};
+        immOut = {{20{instIn[31]}}, instIn[19:12], instIn[13], instIn[30:21], 1'b0};
       end
       7'b1100111: begin // JALR
         immOut[11:0] = instIn[31:20];
-              immOut[31:12] = {20{instIn[31]}};
+        immOut[31:12] = {20{instIn[31]}};
       end
       7'b1100011: begin // Branches
         immOut = {{20{instIn[31]}}, instIn[7], instIn[30:25], instIn[11:8], 1'b0};
@@ -35,12 +35,12 @@ module signExt(opcode, instIn, immOut);
         immOut = {{20{instIn[31]}}, instIn[31:20]};
       end
       7'b0010011: begin // IR-Type
-        immOut = (instIn[14:12] == (3'b001 || 3'b101)) : ({{27'b0, instIn[24:20]}) ? {{20{instIn[31]}}, instIn[31:20]};
+        immOut = (instIn[14:12] == (3'b001 || 3'b101)) : {27'b0, instIn[24:20]} ? {{20{instIn[31]}}, instIn[31:20]};
       end
       7'b0100011: begin
-              immOut = {{20{instIn[31]}}, instIn[31:25], instIn[11:7]};
+        immOut = {{20{instIn[31]}}, instIn[31:25], instIn[11:7]};
       end
       default:
-        immOut = 32'bX;
+        immOut = 32'b0;
     endcase
 endmodule
