e1015fad25d82370470db43d41d2f475560e8c41
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 13:05:59 +0530

    condition s = 11 now outputs 0s

--- src/mux31.sv
+++ src/mux31.sv
@@ -5,5 +5,5 @@ module mux31 #(parameter N = 32) (a, b, c, s, y);
   input logic [N-1:0] c; //10
   input logic [1:0] s;
   output logic [N-1:0] y;
-  assign y = s[1] ? c : (s[0] ? b : a);
+  assign y = s[1] ? (s[0] ? 32'b0 : c) : (s[0] ? b : a);
 endmodule
