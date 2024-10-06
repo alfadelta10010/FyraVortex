6e0f06db96256d72cfeea8945dcb363e7ed12a35
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-06 13:55:31 +0530

    fixed fn7 for shifts

--- src/decoder.sv
+++ src/decoder.sv
@@ -32,73 +32,54 @@ module decoder(instIn, opcode, rd, rs1, rs2, fn3, fn7);
   always_comb
     begin
       opcode = instIn[6:0];
+      {rd, rs1, rs2, fn3, fn7} = 'b0;
       case(opcode)
         7'b0110111: //LUI
           begin
             rd = instIn[11:7];
-            rs1 = 5'bX;
-            rs2 = 5'bX;
-            fn3 = 3'bX;
-            fn7 = 7'bX;
           end
         7'b0010111: //JAL
           begin
             rd = instIn[11:7];
-            rs1 = 5'bX;
-            rs2 = 5'bX;
-            fn3 = 3'bX;
-            fn7 = 7'bX;
           end
         7'b0010111: //AUIPC
           begin
             rd = instIn[11:7];
-            rs1 = 5'bX;
-            rs2 = 5'bX;
-            fn3 = 3'bX;
-            fn7 = 7'bX;
           end
         7'b1100111: //JALR
           begin
             rd = instIn[11:7];
             rs1 = instIn[19:15];
-            rs2 = 5'bX;
             fn3 = instIn[14:12];
-            fn7 = 7'bX;
           end
         7'b1100011: //B-type
           begin
-            rd = 5'bX;
             rs1 = instIn[19:15];
             rs2 = instIn[24:20];
             fn3 = instIn[14:12];
-            fn7 = 7'bX;
           end
-        7'b0000011: //Load-type
+        7'b0000011: //IL-type
           begin 
             rs1 = instIn[19:15];
             rs2 = instIn[24:20];
             rd = instIn[11:7];
             fn3 = instIn[14:12];
-            fn7 = 7'bX;  
           end
         7'b0100011: //S-type
           begin
             rs1 = instIn[19:15];
             rs2 = instIn[24:20];
-            rd = 5'bX;
             fn3 = instIn[14:12];
-            fn7 = 7'bX;  
           end
-        7'b0010011: //I-type
+        7'b0010011: //IR-type
           begin
             rs1 = instIn[19:15];
-            rs2 = 5'bX;
             rd = instIn[11:7];
             fn3 = instIn[14:12];
             if(fn3 == 3'b101)
-              fn7 = instIn[31:24];
+              fn7 = {instIn[31:25], 5'b0};
             else
-              fn7 = 7'bX;
+              fn7 = 7'b0;
           end
         7'b0110011: //R-type
           begin
@@ -110,11 +91,11 @@ module decoder(instIn, opcode, rd, rs1, rs2, fn3, fn7);
           end
         default:
           begin
-            rs1 = 5'bX;
-            rs2 = 5'bX;
-            rd = 5'bX;
-            fn3 = 3'bX;
-            fn7 = 7'bX;
+            rs1 = 5'b0;
+            rs2 = 5'b0;
+            rd = 5'b0;
+            fn3 = 3'b0;
+            fn7 = 7'b0;
           end
       endcase
     end
