b5a3e988d5e4cf3bf32154346164af03f976cac6
Abhiram Dasika <alfadelta10010@gmail.com>
2024-10-25 13:11:19 +0530

    vivado stuff

---
+++ Pipeline-4-stage.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
@@ -0,0 +1,27 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Fri Oct 25 13:06:03 2024
+// Host        : abhirams-popbook-pro running 64-bit Pop!_OS 22.04 LTS
+// Command     : write_verilog -force -mode synth_stub
+//               /home/alphadelta1803/projects/RV32I-4Stage/Pipeline-4-stage.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.v
+// Design      : blk_mem_gen_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7a35tcpg236-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "blk_mem_gen_v8_4_3,Vivado 2019.1" *)
+module blk_mem_gen_0(clka, rsta, ena, wea, addra, dina, douta, rsta_busy)
+/* synthesis syn_black_box black_box_pad_pin="clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],rsta_busy" */;
+  input clka;
+  input rsta;
+  input ena;
+  input [3:0]wea;
+  input [31:0]addra;
+  input [31:0]dina;
+  output [31:0]douta;
+  output rsta_busy;
+endmodule
---
+++ Pipeline-4-stage.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
@@ -0,0 +1,37 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+-- Date        : Fri Oct 25 13:06:03 2024
+-- Host        : abhirams-popbook-pro running 64-bit Pop!_OS 22.04 LTS
+-- Command     : write_vhdl -force -mode synth_stub
+--               /home/alphadelta1803/projects/RV32I-4Stage/Pipeline-4-stage.srcs/sources_1/ip/blk_mem_gen_0/blk_mem_gen_0_stub.vhdl
+-- Design      : blk_mem_gen_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity blk_mem_gen_0 is
+  Port ( 
+    clka : in STD_LOGIC;
+    rsta : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    wea : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    dina : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 31 downto 0 );
+    rsta_busy : out STD_LOGIC
+  );
+
+end blk_mem_gen_0;
+
+architecture stub of blk_mem_gen_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,rsta,ena,wea[3:0],addra[31:0],dina[31:0],douta[31:0],rsta_busy";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_3,Vivado 2019.1";
+begin
+end;
